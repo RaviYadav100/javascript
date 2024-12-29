09b4ddc595fa781790580f448d9ca0c13a3d51e1
RaviYadav100 <114805682+RaviYadav100@users.noreply.github.com>
2024-12-29 19:24:37 +0000

    submit

---
+++ javascript/variable.js
@@ -0,0 +1,26 @@
+const Id="100"
+let email="ravi@gmail.com"
+var pass="12345"
+city="kanpur"
+let state;
+console.table([Id,email,pass,city,state]);
+
+//---------------------variable conversion-----------------------------------------//
+
+let score="100"
+console.log(typeof score);
+let num=Number(score)
+console.log(typeof num);
+console.log(num);
+
+let boolean=1
+console.log(typeof boolean);
+let bool=Boolean(boolean)
+console.log(typeof bool);
+console.log(bool);
+
+let number=109
+console.log(typeof number);
+let string=String(number)
+console.log(typeof string);
+console.log(string);
